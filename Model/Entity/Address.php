d9b5f1f114ad47b27e2ef26f322414aa5e885ba1
dodkirua <pybouttefeux@gmail.com>
2021-05-31 10:50:55 +0200

    add AddressBook.php et Address.php

---
+++ Model/Entity/Address.php
@@ -0,0 +1,149 @@
+<?php
+
+
+namespace Model\Entity;
+
+use Model\Entity\Interfaces\EntityInterface;
+
+class Address extends Entity implements EntityInterface{
+    private ?int $num;
+    private ?string $street;
+    private ?int $zip;
+    private ?string $city;
+    private ?string $country;
+    private ?string $add;
+
+    public function __construct(int $id = null ,?int $num, ?string $street, ?int $zip, ?string $city, ?string $country,
+    ?string $add) {
+        parent::__construct($id);
+        $this->$num = $num;
+        $this->street = $street;
+        $this->zip = $zip;
+        $this->city = $city;
+        $this->country = $country;
+        $this->add = $add;
+    }
+
+    /**
+     * get the Num
+     * @return int|null
+     */
+    public function getNum(): ?int    {
+        return $this->num;
+    }
+
+    /**
+     * set the Num
+     * @param int|null $num
+     * @return Address
+     */
+    public function setNum(?int $num): Address    {
+        $this->num = $num;
+        return $this;
+    }
+
+    /**
+     * get the Street
+     * @return string|null
+     */
+    public function getStreet(): ?string    {
+        return $this->street;
+    }
+
+    /**
+     * set the Street
+     * @param string|null $street
+     * @return Address
+     */
+    public function setStreet(?string $street): Address    {
+        $this->street = $street;
+        return $this;
+    }
+
+    /**
+     * get the Zip
+     * @return int|null
+     */
+    public function getZip(): ?int    {
+        return $this->zip;
+    }
+
+    /**
+     * set the Zip
+     * @param int|null $zip
+     * @return Address
+     */
+    public function setZip(?int $zip): Address    {
+        $this->zip = $zip;
+        return $this;
+    }
+
+    /**
+     * get the City
+     * @return string|null
+     */
+    public function getCity(): ?string    {
+        return $this->city;
+    }
+
+    /**
+     * set the City
+     * @param string|null $city
+     * @return Address
+     */
+    public function setCity(?string $city): Address    {
+        $this->city = $city;
+        return $this;
+    }
+
+    /**
+     * get the Country
+     * @return string|null
+     */
+    public function getCountry(): ?string    {
+        return $this->country;
+    }
+
+    /**
+     * set the Country
+     * @param string|null $country
+     * @return Address
+     */
+    public function setCountry(?string $country): Address    {
+        $this->country = $country;
+        return $this;
+    }
+
+    /**
+     * get the Add
+     * @return string|null
+     */
+    public function getAdd(): ?string    {
+        return $this->add;
+    }
+
+    /**
+     * set the Add
+     * @param string|null $add
+     * @return Address
+     */
+    public function setAdd(?string $add): Address    {
+        $this->add = $add;
+        return $this;
+    }
+
+    /**
+     * return all the value of object
+     * @return array
+     */
+    public function getAllData(): array    {
+        $array['id'] = $this->getId();
+        $array['num'] = $this->getNum();
+        $array['street'] = $this->getStreet();
+        $array['zip_code'] = $this->getZip();
+        $array['city'] = $this->getCity();
+        $array['country'] = $this->getCountry();
+        $array['add'] = $this->getAdd();
+        return $array;
+    }
+}
---
+++ Model/Entity/AddressBook.php
@@ -0,0 +1,66 @@
+<?php
+
+
+namespace Model\Entity;
+
+use Model\Entity\Interfaces\EntityInterface;
+use Model\Entity\User;
+use Model\Entity\Address;
+
+class AddressBook extends Entity implements EntityInterface{
+    private ?User $user;
+    private ?Address $address;
+
+    public function __construct(int $id = null ,User $user = null, Address $address = null){
+        parent::__construct($id);
+        $this->user = $user;
+        $this->address = $address;
+    }
+
+    /**
+     * get the User
+     * @return User|null
+     */
+    public function getUser(): ?User    {
+        return $this->user;
+    }
+
+    /**
+     * set the User
+     * @param User|null $user
+     * @return AddressBook
+     */
+    public function setUser(?User $user): AddressBook    {
+        $this->user = $user;
+        return $this;
+    }
+
+    /**
+     * get the Address
+     * @return Address|null
+     */
+    public function getAddress(): ?Address    {
+        return $this->address;
+    }
+
+    /**
+     * set the Address
+     * @param Address|null $address
+     * @return AddressBook
+     */
+    public function setAddress(?Address $address): AddressBook    {
+        $this->address = $address;
+        return $this;
+    }
+
+    /**
+     * return all the value of object
+     * @return array
+     */
+    public function getAllData(): array    {
+        $array['id'] = $this->getId();
+        $array['user_id'] = $this->getUser()->getAllData();
+        $array['address_id'] = $this->getAddress()->getAllData();
+        return $array;
+    }
+}
