5999a7b590bc23d5cd276cfb997e377dae1a4b3d
dodkirua <pybouttefeux@gmail.com>
2021-05-31 16:02:50 +0200

    CategoryAgeArrayManager.php

---
+++ Model/Manager/CategoryAgeArrayManager.php
@@ -0,0 +1,151 @@
+<?php
+
+
+namespace Model\Manager;
+
+use Model\DB;
+use PDOStatement;
+use Model\CategoryAgeArray;
+use Model\Manager\UserManager;
+use Model\Entity\User;
+use Model\Manager\CategoryAgeManager;
+use Model\Entity\CategoryAge;
+
+class CategoryAgeArrayManager extends Manager{
+    /**
+     * return a CategoryAgeArray by id
+     * @param int $id
+     * @return CategoryAgeArray|null
+     */
+    public function getById (int $id) : ?CategoryAgeArray{
+        $request = DB::getInstance()->prepare("SELECT * From category_age_array where id = :id");
+        $request->bindValue(":id",$id);
+        return $this->getOne($request);
+    }
+
+    /**
+     * get all CategoryAgeArray by categorie age
+     * @param int $categorieAge
+     * @return array
+     */
+    public function getAllByCategorieAge(int $categorieAge) : array{
+        $request = DB::getInstance()->prepare("SELECT * From category_age_array where category_age_id = :cat");
+        $request->bindValue(":cat",$categorieAge);
+        return $this->getMany($request);
+    }
+
+    /**
+     * get all CategoryAgeArray by user
+     * @param int $user
+     * @return array
+     */
+    public function getAllByUser(int $user) : array{
+        $request = DB::getInstance()->prepare("SELECT * From category_age_array where user_id = :user");
+        $request->bindValue(":user",$user);
+        return $this->getMany($request);
+    }
+
+    /**
+     * return a array with all the CategoryAgeArray
+     * @return array
+     */
+    public function getAll() : array {
+        $request = DB::getInstance()->prepare("SELECT * From category_age_array");
+        return $this->getMany($request);
+    }
+
+    /**
+     * update on DB with id
+     * @param int $id
+     * @param int|null $userId
+     * @param int|null $categoryAgeId
+     * @return bool
+     */
+    public function update(int $id, int $userId= null, int $categoryAgeId = null) : bool{
+        if (is_null($userId) || is_null($categoryAgeId)) {
+            $data = $this->getById($id);
+            if (is_null($userId)) {
+                $userId = $data->getUser()->getId();
+            }
+            if (is_null($categoryAgeId)) {
+                $categoryAgeId = $data->getCategoryAge()->getId();
+            }
+        }
+
+        $request = DB::getInstance()->prepare("UPDATE category_age_array
+                    SET user_id = :user, category_age_id = :cat
+                    WHERE id= :id
+                    ");
+        $request->bindValue(":id",$id);
+        $request->bindValue(":user",$userId);
+        $request->bindValue(":cat",$categoryAgeId);
+
+        return $request->execute();
+    }
+
+    /**
+     * insert CategoryAgeArray in DB
+     * @param int $userId
+     * @param $categoryAgeId
+     * @return bool
+     */
+    public function add(int $userId, $categoryAgeId) : bool {
+        $request = DB::getInstance()->prepare("INSERT INTO category_age_array 
+        (user_id, category_age_id)
+        VALUES (:user, :cat)
+        ");
+        $request->bindValue(":user",$userId);
+        $request->bindValue(":cat",$categoryAgeId);
+
+        return $request->execute();
+    }
+
+    /**
+     * delete CategoryAgeArray by id
+     * @param int $id
+     * @return bool
+     */
+    public function delete(int $id) : bool {
+        $request = DB::getInstance()->prepare("DELETE From category_age_array WHERE id = :id");
+        $request->bindValue(':id',$id);
+        return $request->execute();
+    }
+
+
+    /**
+     * private request for the getBy
+     * @param PDOStatement $request
+     * @return CategoryAgeArray|null
+     */
+    private function getOne(PDOStatement $request ) : ?CategoryAgeArray {
+        $request->execute();
+        $data = $request->fetch();
+        if ($data) {
+            return new CategoryAgeArray (intval($data['id']), $data['title'], $data['content']);
+        }
+        return null;
+    }
+
+    /**
+     * private request for the getAll
+     * @param PDOStatement $request
+     * @return array
+     */
+    private function getMany (PDOStatement $request ) : array {
+        $array = [];
+        $result = $request->execute();
+
+        if ($result){
+            $data = $request->fetchAll();
+            if ($data) {
+                foreach ($data as $datum) {
+                    $user = (new UserManager())->getById(intval($datum['user_id']));
+                    $categoryAge = (new CategoryAgeManager())->getById(intval($datum['category_age_id']));
+                    $item = new CategoryAgeArray(intval($datum['id']), $user, $categoryAge);
+                    $array[] = $item;
+                }
+            }
+        }
+        return $array;
+    }
+}
--- Model/Manager/LessonManager.php
+++ Model/Manager/LessonManager.php
@@ -7,7 +7,120 @@ use Model\DB;
 use PDOStatement;
 use Model\Entity\Lesson;
 
-class LessonManager extends Manager
-{
+class LessonManager extends Manager{
+    /**
+     * return a Lesson by id
+     * @param int $id
+     * @return Lesson|null
+     */
+    public function getById (int $id) : ?Lesson{
+        $request = DB::getInstance()->prepare("SELECT * FROM lesson where id = :id");
+        $request->bindValue(":id",$id);
+        return $this->getOne($request);
+    }
+
+    /**
+     * get a Lesson by title
+     * @param string $title
+     * @return Lesson|null
+     */
+    public function getByTitle (string $title) : ?Lesson{
+        $request = DB::getInstance()->prepare("SELECT * FROM lesson where title = :title");
+        $request->bindValue(":title",$title);
+        return $this->getOne($request);
+    }
+
+    /**
+     * return a array with all the lesson
+     * @return array
+     */
+    public function getAll() : array {
+        $array = [];
+        $request = DB::getInstance()->prepare("SELECT * FROM lesson");
+        $result = $request->execute();
+
+        if ($result){
+            $data = $request->fetchAll();
+            if ($data) {
+                foreach ($data as $datum) {
+                    $item = new Lesson(intval($datum['id']), $datum['title'], $datum['content']);
+                    $array[] = $item;
+                }
+            }
+        }
+        return $array;
+    }
+
+    /**
+     * update on DB with id
+     * @param int $id
+     * @param string|null $title
+     * @param string|null $content
+     * @return bool
+     */
+    public function update(int $id, string $title = null, string $content = null) : bool{
+        if (is_null($title) || is_null($content)) {
+            $data = $this->getById($id);
+            if (is_null($title)) {
+                $title = $data->getTitle();
+            }
+            if (is_null($content)) {
+                $content = $data->getContent();
+            }
+        }
+
+        $request = DB::getInstance()->prepare("UPDATE lesson
+                    SET title = :title, content = :content
+                    WHERE id= :id
+                    ");
+        $request->bindValue(":id",$id);
+        $request->bindValue(":title",mb_strtolower($title));
+        $request->bindValue(":content",mb_strtolower($content));
+
+        return $request->execute();
+    }
+
+    /**
+     * insert lesson in DB
+     * @param string $title
+     * @param string $content
+     * @return bool
+     */
+    public function add(string $title, string $content) : bool {
+        $request = DB::getInstance()->prepare("INSERT INTO lesson 
+        (title, content)
+        VALUES (:title, :content)
+        ");
+        $request->bindValue(":title",mb_strtolower($title));
+        $request->bindValue(":content",mb_strtolower($content));
+
+        return $request->execute();
+    }
+
+    /**
+     * delete lesson by id
+     * @param int $id
+     * @return bool
+     */
+    public function delete(int $id) : bool {
+        $request = DB::getInstance()->prepare("DELETE FROM lesson WHERE id = :id");
+        $request->bindValue(':id',$id);
+        return $request->execute();
+    }
+
+
+    /**
+     * private request for the getBy
+     * @param PDOStatement $request
+     * @return Lesson|null
+     */
+    private function getOne(PDOStatement $request ) : ?Lesson {
+        $request->execute();
+        $data = $request->fetch();
+        if ($data) {
+            return new Lesson (intval($data['id']), $data['title'], $data['content']);
+        }
+        return null;
+    }
 
 }
