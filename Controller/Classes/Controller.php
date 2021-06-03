5ca082ad42f6ed69c554cc27fe1073a35032df04
dodkirua <pybouttefeux@gmail.com>
2021-06-03 13:36:23 +0200

    css

--- Controller/Classes/Controller.php
+++ Controller/Classes/Controller.php
@@ -6,7 +6,7 @@ namespace Controller\Classes;
 
 class Controller{
 
-    protected static function render(string $view, string $title,string $css, array $var = null) {
+    protected static function render(string $view, string $title, array $var = null) {
         ob_start();
         require_once $_SERVER['DOCUMENT_ROOT'] . "/View/$view.view.php";
         $html = ob_get_clean();
